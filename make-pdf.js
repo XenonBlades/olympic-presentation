ab6ba6ea87cb2112aab45289b96f11c52562b986
Ayush Dey <ayushxe54@gmail.com>
2026-07-07 15:08:12 +0900

    Fix Puppeteer crash in GitHub Actions by disabling sandbox

--- make-pdf.js
+++ make-pdf.js
@@ -13,7 +13,9 @@ const SLIDES = [
 ];
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+	  args: ['--no-sandbox', '--disable-setuid-sandbox']
+	});
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
 
